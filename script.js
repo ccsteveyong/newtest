b10bc86f952c45460a710b4cd478cb5d7dae60eb
ccsteveyong <jakesteveyong@gmail.com>
2026-07-19 17:47:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const API = "https://script.google.com/macros/s/AKfycbyS2-C9UbYSoFpTJmDoBpjnKwdqRynm3mx-cDcKxKSKR8Y6MaODCtMNjtzAmlyTvLY1/exec";
 
 
+function numberWithZero(num){
+    return num < 10 ? "0"+num : num;
+}
+
+
 fetch(API)
 
 .then(response => response.json())
 
 .then(data => {
+
+
+console.log("API Loaded:", data);
+
 
 
 const bgWrapper =
@@ -16,6 +25,8 @@ const titleWrapper =
 document.querySelector(".swiper-wrapper.is-slider-titles");
 
 
+// Remove Webflow placeholders
+
 bgWrapper.innerHTML="";
 titleWrapper.innerHTML="";
 
@@ -24,39 +35,60 @@ titleWrapper.innerHTML="";
 data.forEach(item=>{
 
 
-bgWrapper.insertAdjacentHTML("beforeend",`
+// IMAGE SLIDE
 
+bgWrapper.insertAdjacentHTML(
+"beforeend",
+
+`
 <div class="swiper-slide">
 
-<img src="${item.image}" class="slider-bg_img">
+<img 
+src="${item.image}"
+class="slider-bg_img">
 
 </div>
+`
 
-`);
+);
 
 
 
-titleWrapper.insertAdjacentHTML("beforeend",`
 
+// TITLE SLIDE
+
+titleWrapper.insertAdjacentHTML(
+"beforeend",
+
+`
 <div class="swiper-slide">
 
 <div class="year">
 ${item.year}
 </div>
 
+
 <p class="slider-titles_heading">
 ${item.title}
 </p>
 
 </div>
+`
 
-`);
+);
+
 
 });
 
 
 
-const bgSwiper = new Swiper(".swiper.is-slider-bg",{
+
+
+// IMAGE SLIDER
+
+const bgSwiper = new Swiper(
+".swiper.is-slider-bg",
+{
 
 slidesPerView:1,
 
@@ -64,33 +96,96 @@ speed:400,
 
 effect:"fade",
 
-allowTouchMove:false
-
-});
+allowTouchMove:false,
 
 
+}
+);
 
-const textSwiper = new Swiper(".swiper.is-slider-titles",{
+
+
+
+
+
+// TITLE SLIDER
+
+const textSwiper = new Swiper(
+".swiper.is-slider-titles",
+{
 
 slidesPerView:"auto",
 
 speed:600,
 
+
+loop:true,
+
+loopedSlides:data.length,
+
+
 centeredSlides:true,
 
-navigation:{
-nextEl:".swiper-next",
-prevEl:".swiper-prev"
+
+slideActiveClass:"is-active",
+
+slideDuplicateActiveClass:"is-active",
+
+
+
+slideToClickedSlide:true,
+
+
+mousewheel:true,
+
+
+keyboard:{
+enabled:true,
+onlyInViewport:true
 },
 
-slideActiveClass:"is-active"
+
+
+navigation:{
+
+nextEl:".swiper-next",
+
+prevEl:".swiper-prev"
+
+}
+
+
+}
+
+);
+
+
+
+
+
+// CONNECT IMAGE + TEXT
+
+textSwiper.controller.control = bgSwiper;
+
+bgSwiper.controller.control = textSwiper;
+
+
+
+
+
+// UPDATE COUNTER
+
+textSwiper.on("slideChange", function(e){
+
+document.querySelector(".swiper-number-current").textContent =
+numberWithZero(e.realIndex + 1);
 
 });
 
 
 
-textSwiper.controller.control = bgSwiper;
-bgSwiper.controller.control = textSwiper;
+document.querySelector(".swiper-number-total").textContent =
+numberWithZero(data.length);
+
 
 
 });
