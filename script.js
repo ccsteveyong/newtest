ca8f2ac19df186a77cbe1ebce128e3b3c03bda53
ccsteveyong <jakesteveyong@gmail.com>
2026-07-19 16:44:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
 const API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnS0CJyg0GCJHzOcI28Mc70EDiUea3kWU9QtzeaJAZT_vJ1ZVZejSWJTQt_zqW7jANs2rVvwdONlVeUCAgD236xWz6tzbKJkfYDfYkxVwmG7KMCdJuEKUJPNPcS8MJqcR8p5GyH2nb6eXiChLK3ryl59ByPuKhgnBnlyeAp5ZGKyaafrUOMtqtx7Tuq6_Crf5u5QRQHaiaPUDNqSBqU9ozvjeAqmI134sWj-ZdvqtD0Oz8LQZMNaagFxZqGk3MSUiwY4tNcjNT-NkWCJUNOcD7_QSC48TA&lib=M4Bv-ARtfVX03mmcIP3dWBKV8G9BMDobo";
 
 fetch(API)
-
 .then(res => res.json())
-
 .then(data => {
 
-    const timeline = document.getElementById("timeline");
+    const item = data[0];
 
-    data.forEach(item => {
+    document.getElementById("year").textContent = item.year;
 
-        const div = document.createElement("div");
+    document.getElementById("title").textContent = item.title;
 
-        div.className = "timeline-item";
+    document.getElementById("image").src = item.image;
 
-        div.innerHTML = `
-        
-            <img class="timeline-image" src="${item.image}" alt="${item.title}">
+    const list = document.getElementById("content-list");
 
-            <div class="year">${item.year}</div>
+    item.contents.forEach(content => {
 
-            <div class="title">${item.title}</div>
-
-            <ul>
-
-            ${
-                item.contents
-                ?
-                item.contents
-                    .map(text => `<li>${text}</li>`)
-                    .join("")
-                :
-                ""
-            }
-
-            </ul>
-
-        `;
-
-        timeline.appendChild(div);
+        list.innerHTML += `<li>${content}</li>`;
 
     });
 
