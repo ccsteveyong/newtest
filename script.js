5689edfdf46d168ebdec074d687645233714cb18
ccsteveyong <jakesteveyong@gmail.com>
2026-07-19 17:44:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 const API = "https://script.google.com/macros/s/AKfycbyS2-C9UbYSoFpTJmDoBpjnKwdqRynm3mx-cDcKxKSKR8Y6MaODCtMNjtzAmlyTvLY1/exec";
 
 
-function numberWithZero(num){
-    return num < 10 ? "0"+num : num;
-}
-
-
 fetch(API)
 
 .then(response => response.json())
 
 .then(data => {
-
-
-console.log(data);
-
 
 
 const bgWrapper =
@@ -25,8 +16,6 @@ const titleWrapper =
 document.querySelector(".swiper-wrapper.is-slider-titles");
 
 
-// Remove Webflow placeholder
-
 bgWrapper.innerHTML="";
 titleWrapper.innerHTML="";
 
@@ -35,102 +24,73 @@ titleWrapper.innerHTML="";
 data.forEach(item=>{
 
 
-// IMAGE
+bgWrapper.insertAdjacentHTML("beforeend",`
 
-bgWrapper.insertAdjacentHTML(
-"beforeend",
+<div class="swiper-slide">
 
-`
-<div class="swiper-slide is-slider-bg">
-
-<img 
-src="${item.image}"
-class="slider-bg_img">
+<img src="${item.image}" class="slider-bg_img">
 
 </div>
-`
 
-);
-
+`);
 
 
 
-// TEXT
+titleWrapper.insertAdjacentHTML("beforeend",`
 
-titleWrapper.insertAdjacentHTML(
-"beforeend",
-
-`
-<div class="swiper-slide is-slider-titles">
-
+<div class="swiper-slide">
 
 <div class="year">
 ${item.year}
 </div>
 
-
 <p class="slider-titles_heading">
 ${item.title}
 </p>
 
-
 </div>
-`
 
-);
-
+`);
 
 });
 
 
 
-// INIT SWIPER AFTER DATA EXISTS
+const bgSwiper = new Swiper(".swiper.is-slider-bg",{
 
-
-const bgSwiper = new Swiper(
-".swiper.is-slider-bg",
-{
 slidesPerView:1,
+
 speed:400,
+
 effect:"fade",
+
 allowTouchMove:false
-}
-);
+
+});
 
 
 
-const textSwiper = new Swiper(
-".swiper.is-slider-titles",
-{
+const textSwiper = new Swiper(".swiper.is-slider-titles",{
 
 slidesPerView:"auto",
 
 speed:600,
 
-loop:true,
-
-loopedSlides:data.length,
-
 centeredSlides:true,
 
 navigation:{
-
 nextEl:".swiper-next",
-
 prevEl:".swiper-prev"
-
 },
 
 slideActiveClass:"is-active"
 
-}
-
-);
+});
 
 
 
 textSwiper.controller.control = bgSwiper;
-
+bgSwiper.controller.control = textSwiper;
 
 
 });
